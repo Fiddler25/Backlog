407652c584113bd6fe9e3c1f100c07cc3d866383
AtsushiKanayama <rager.mka.ti@icloud.com>
2020-08-02 17:30:29 +0900

    product_back_log一覧画面作成

---
+++ backlog-front/components/ProductBackLogsContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul v-for="product_back_log in this.$parent.product_back_logs" :key="product_back_log.id">
+      <li>
+        <h2>{{ product_back_log.title }}</h2>
+        <h3>完了の定義: {{ product_back_log.completion_definition }}</h3>
+        <h3>説明: {{ product_back_log.description }}</h3>
+        <h3>Story Point: {{ product_back_log.story_point }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ backlog-front/pages/productBackLog/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ProductBackLogsContainer />
+  </div>
+</template>
+
+<script>
+import ProductBackLogsContainer from '~/components/ProductBackLogsContainer.vue'
+export default {
+  components: {
+    ProductBackLogsContainer
+  },
+  data() {
+    return {
+      product_back_logs: []
+    }
+  },
+  async asyncData({ $axios }) {
+    const data = await $axios.$get('/api/product_back_logs')
+    return { product_back_logs: data }
+  }
+}
+</script>
